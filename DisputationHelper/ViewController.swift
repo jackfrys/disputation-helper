1e3f8b5f10f64fc77d87439fb253bcd867b5d327
Jack Frysinger <jcf0810@me.com>
2016-12-01 04:30:53 -0500

    Initial commit

---
+++ DisputationHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DisputationHelper
+//
+//  Created by Jack Frysinger on 12/1/16.
+//  Copyright © 2016 Jack Frysinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
