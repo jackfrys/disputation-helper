784e50352beb026cec4ac25f8a6f48d6b403c3f5
Jack Frysinger <jcf0810@me.com>
2016-12-20 16:23:36 -0500

    renamed to MainViewController

--- DisputationHelper/MainViewController.swift
+++ DisputationHelper/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  DisputationHelper
 //
 //  Created by Jack Frysinger on 12/1/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
