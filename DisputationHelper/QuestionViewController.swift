32ca7a9e99b8879a96162c5a11ae4939e60793dc
Jack Frysinger <jcf0810@me.com>
2016-12-25 19:15:42 -0500

    VCs for question and contradiction views. Segues and back buttons.

---
+++ DisputationHelper/QuestionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  QuestionViewController.swift
+//  DisputationHelper
+//
+//  Created by Jack Frysinger on 12/25/16.
+//  Copyright © 2016 Jack Frysinger. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
